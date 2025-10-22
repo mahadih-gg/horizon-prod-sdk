e86ca516e72757c59684c6a2ea2004423f061de6
mahadih <mahadih.dev@gmail.com>
2025-10-22 23:05:41 +0600

    Update @thinkflagship/web-shorts dependency to version 1.1.14, modify license key in App component, and adjust EntryPoint ID for improved functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
 
   const LICENSE_KEY =
-    "pk_live_7641a6a803379ca11f368336825d69d54e9437ff3ed60125.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUHJvZC1RQSIsInBsYXRmb3JtVHlwZSI6IldFQiIsImRvbWFpbiI6eyJkb21haW4iOiJodHRwczovL2hvcml6b24tcHJvZC1zZGsudmVyY2VsLmFwcC8iLCJ2ZXJpZmllZCI6ZmFsc2V9LCJleHBpcmVzQXQiOiIyMTI1LTA5LTEwVDA2OjM2OjQxLjUyNVoiLCJjcmVhdGVkQnkiOiI2OGUwYjk4OTNjZjA1YTc5OTVlODhkMTUiLCJ0ZW5hbnRJZCI6IjY4ZTBiOTg5M2NmMDVhNzk5NWU4OGQxMSIsIl9pZCI6IjY4ZTBjMDc5Y2E5MDc4NjNkNjhiZDQxYiIsInRva2VuVHlwZSI6ImFjY2VzcyIsImlhdCI6MTc1OTU1OTgwMSwiZXhwIjo0OTEzMTU5ODAxfQ.Cv8hvLSrMiVAmbuqZaJthgp1QkTpdOo0DQKl8CIUdgA";
+    "pk_live_ea76e5f3317a15003940c4317a99ed18fbd3abd29f864519.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUHJvZCBRQSIsInBsYXRmb3JtVHlwZSI6IldFQiIsImRvbWFpbiI6eyJkb21haW4iOiJodHRwczovL2hvcml6b24tcHJvZC1zZGsudmVyY2VsLmFwcCIsInZlcmlmaWVkIjpmYWxzZX0sImV4cGlyZXNBdCI6IjIxMjUtMDktMjhUMTY6NTk6MjcuODczWiIsImNyZWF0ZWRCeSI6IjY4ZTBiOTg5M2NmMDVhNzk5NWU4OGQxNSIsInRlbmFudElkIjoiNjhmOGRkYThlNjNmMGVjYjZjZmY5MjhmIiwiX2lkIjoiNjhmOTBkNmZjNjNhMTNhMzgyMjBiMjAzIiwidG9rZW5UeXBlIjoiYWNjZXNzIiwiaWF0IjoxNzYxMTUyMzY3LCJleHAiOjQ5MTQ3NTIzNjd9.PtGigq_W63foS38hPWLnENSP92ff4qK5bYiNmIm17PA";
 
   return (
     <div className="app">
@@ -129,7 +129,7 @@ function App() {
         </section>
 
         <EntryPoint
-          id="#812890"
+          id="#894017"
           skeletonType={EntryPointType.RECTANGLE}
           skeletonSize={EntryPointSize.BOLD}
         />
